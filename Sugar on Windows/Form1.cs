e9fe3ad7a1ea8dec832deda742c6e2efaa0bbf70
Hrishi <hrishipatel99@gmail.com>
2018-03-01 22:10:04 +0530

    Added basic code

--- Sugar on Windows/Form1.cs
+++ Sugar on Windows/Form1.cs
@@ -16,5 +16,38 @@ namespace Sugar_on_Windows
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            enable_disable(true);
+        }
+
+        private void enable_disable(bool response)
+        {
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            startInfo.FileName = "cmd.exe";
+            if (response == true)
+            {
+                startInfo.Arguments = "dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux";
+            }
+            else
+            {
+                startInfo.Arguments = "dism.exe /online /disable-feature /featurename:Microsoft-Windows-Subsystem-Linux";
+            }
+            process.StartInfo = startInfo;
+            process.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            enable_disable(false);
+        }
     }
 }
