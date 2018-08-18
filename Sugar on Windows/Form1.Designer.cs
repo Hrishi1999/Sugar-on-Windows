d2c71ff61be7336194678592eea9598ce236911b
Hrishi <hrishipatel99@gmail.com>
2018-08-18 19:38:24 +0530

    Basic Functionality ready

--- Sugar on Windows/Form1.Designer.cs
+++ Sugar on Windows/Form1.Designer.cs
@@ -35,6 +35,8 @@
             this.button3 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -42,9 +44,9 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(294, 17);
+            this.label1.Size = new System.Drawing.Size(444, 17);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Enable/Disable Windows Subsystem for Linux";
+            this.label1.Text = "Enable/Disable Windows Subsystem for Linux (Need to run as admin)";
             // 
             // button1
             // 
@@ -79,32 +81,56 @@
             // 
             this.button3.Location = new System.Drawing.Point(15, 143);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(240, 42);
+            this.button3.Size = new System.Drawing.Size(486, 33);
             this.button3.TabIndex = 4;
             this.button3.Text = "Create";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // textBox1
             // 
             this.textBox1.Location = new System.Drawing.Point(15, 115);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(487, 22);
+            this.textBox1.Size = new System.Drawing.Size(395, 22);
             this.textBox1.TabIndex = 5;
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(15, 241);
+            this.button4.Enabled = false;
+            this.button4.Location = new System.Drawing.Point(12, 246);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(487, 42);
             this.button4.TabIndex = 7;
             this.button4.Text = "Start Sugar";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(416, 112);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(85, 25);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "...";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 226);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(491, 17);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "You will need to install a X Server for Windows like VcXsrv. Please refer docs\r\n";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(514, 295);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button3);
@@ -129,6 +155,8 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- Sugar on Windows/Form1.cs
+++ Sugar on Windows/Form1.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,6 +14,13 @@ namespace Sugar_on_Windows
 {
     public partial class Form1 : Form
     {
+
+        public static bool IsAdministrator()
+        {
+            return (new WindowsPrincipal(WindowsIdentity.GetCurrent()))
+                      .IsInRole(WindowsBuiltInRole.Administrator);
+        }
+
         public Form1()
         {
             InitializeComponent();
@@ -19,12 +28,16 @@ namespace Sugar_on_Windows
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            if (!IsAdministrator())
+            {
+                button1.Enabled = false;
+                button2.Enabled = false;
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            enable_disable(true);
+            enable_disable(true);       
         }
 
         private void enable_disable(bool response)
@@ -32,14 +45,17 @@ namespace Sugar_on_Windows
             System.Diagnostics.Process process = new System.Diagnostics.Process();
             System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
             startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
-            startInfo.FileName = "cmd.exe";
-            if (response == true)
+            startInfo.FileName = "powershell.exe";
+            if (response)
             {
-                startInfo.Arguments = "dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux";
+                startInfo.Arguments = "Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux";
+                MessageBox.Show("Feature enabled");
             }
             else
             {
-                startInfo.Arguments = "dism.exe /online /disable-feature /featurename:Microsoft-Windows-Subsystem-Linux";
+                startInfo.Arguments = "Disable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux";
+                MessageBox.Show("Feature disabled");
+
             }
             process.StartInfo = startInfo;
             process.Start();
@@ -49,5 +65,38 @@ namespace Sugar_on_Windows
         {
             enable_disable(false);
         }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            using (var folderDialog = new FolderBrowserDialog())
+            {
+                if (folderDialog.ShowDialog() == DialogResult.OK)
+                {
+                    textBox1.Text = folderDialog.SelectedPath + "\\sugar.bat"; 
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            String data = @"cd C:\Windows\System32\powershell.exe 
+bash -c -i sugar";
+
+            TextWriter txt = new StreamWriter(textBox1.Text);
+            txt.Write(data);
+            txt.Close();
+            button4.Enabled = true;
+            MessageBox.Show(@"You can now run the batch file (.bat) or click on ""Start Sugar");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                System.Diagnostics.Process.Start(@"config.xlaunch");
+                System.Diagnostics.Process.Start(textBox1.Text);
+            }
+        }
     }
-}
+
+ }
